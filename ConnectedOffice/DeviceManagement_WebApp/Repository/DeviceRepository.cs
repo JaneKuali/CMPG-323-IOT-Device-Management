2bdec0061682157acc0dc3f5a84f570609d18d6d
Jane Mpho Kuali <janemphokuali@gmail.com>
2022-09-23 15:16:53 +0200

    implement the device interface

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp.Data;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class DeviceRepository : GenericRepository<Device>, IDeviceRepository
+    {
+        public DeviceRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+    }
+}
