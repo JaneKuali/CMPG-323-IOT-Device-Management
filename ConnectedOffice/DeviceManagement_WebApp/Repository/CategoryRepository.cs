95e87251215a04e8459c2cc0f780a4bb5c833a1a
Jane Mpho Kuali <janemphokuali@gmail.com>
2022-09-28 20:12:59 +0200

    implement cateogory entity

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp.Data;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+    }
+}
