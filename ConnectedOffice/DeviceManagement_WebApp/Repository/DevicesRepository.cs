441193f5a3f06a49d5ac3e63ef725121c1c3989e
Jane Mpho Kuali <janemphokuali@gmail.com>
2022-09-28 20:16:42 +0200

    implement the devices entity

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DevicesRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp.Data;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class DevicesRepository : GenericRepository<Device>, IDevicesRepository
+    {
+        public DevicesRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+    }
+}
